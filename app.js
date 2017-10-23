63a28868122a8540a048fbdfe7e69b43bf6f864a
J3ffcon1 <Jrlone1990@gmail.com>
2017-10-23 12:57:26 -0700

    added constructor function and Data object

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-var Data = function(Projects, projectnames) {
+var Data = [
+{
+  title: 'Guessing-Game',
+  course: 'Code 201',
+  description: 'this project is a guessing game that uses javascript to quiz people on trivia questions about me.',
+},
 
+{
+  title: 'Cookie-stand',
+  course:  'Code 201',
+  description: 'Created a website for a cookie stand that uses randomly generated data to calculate totals of cookies sold for different store branches.',
+},
 
+{
+  title: 'Market Analysis App',
+  course:  'Code 201',
+  description: 'Use Canvas API to create a table that would show data aquired from a clicking test, which asks participants to click the image of a product which most appeals to them',
+},
+{
+  title: 'Stop and Drop',
+  course: 'Code 201',
+  description: 'Used Google API to create a crowdsourced app that locates restrooms, aimed at people who drive for a living or those who just need to find the closest bathroom',
+}
+];
+
+var Portfolioprojects = function (title, course, description) {
+  this.title = title;
+  this.course = course;
+  this.description = description;
 };
