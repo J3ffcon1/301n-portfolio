26dad148215da92c6a736cb88e8d57851819218e
J3ffcon1 <Jrlone1990@gmail.com>
2017-11-05 15:23:09 -0800

    added handleMainNav function

---
+++ pageView.js
@@ -0,0 +1,13 @@
+var handleMainNav = function() {
+console.log('called handleMainNav function');
+
+$('#menu').on('click','.tab', function(event) {
+  event.preventDefault();
+  var selectedTab = '#' + $(this).data('content');
+  console.log('showing ' + selectedTab);
+  $('.tab-content').hide();
+  $(selectedTab).show();
+});
+
+  $('.menu .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
+};
