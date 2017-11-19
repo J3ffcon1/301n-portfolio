0d53119f4c917c9e0fb0ae0c1aee8f1068875e58
J3ffcon1 <Jrlone1990@gmail.com>
2017-11-19 14:43:32 -0800

    Added JSON file and used AJAX call and JQUERY to load data to index.html

--- portfolio.js
+++ portfolio.js
@@ -5,38 +5,35 @@ function Portfolioproject (project) {
   this.course = project.course;
   this.description = project.description;
 };
-Portfolio.all = [];
+Portfolioproject.all = [];
 
 Portfolioproject.prototype.toHtml = function () {
   var articleTemplate = $("#project-template").html();
   var theTemplate = Handlebars.compile(articleTemplate);
-  // var $articleTemplate = $('.template').clone().removeClass('template');
-  // $articleTemplate.find('h1').text(this.title);
-  // $articleTemplate.find('.course p').text(this.course);
-  // $articleTemplate.find('.description p').text(this.description);
 
-  // return $articleTemplate;
   return theTemplate(this);
 };
 
-data.forEach(function (data){
-     var newArticle = new Portfolioproject(data);
-    $('#projects').append(newArticle.toHtml());
-  });
 
-  Portfolio.fetchAll =function () {
-    if (localStorage.project) {
-      Portfolioproject.loadAll(JSON.parse(localStorage.project));
+Portfolioproject.fetchAll =function () {
+  if (localStorage.project) {
+    Portfolioproject.loadAll(JSON.parse(localStorage.getItem("project")));
+    portfolioView.initPage();
+
+  } else {
+    console.log("localStorage does not exist");
+    $.getJSON("/data/portfolio.json", function(data){
+      localStorage.setItem ('project', JSON.stringify(data));
+      Portfolioproject.loadAll(data);
       portfolioView.initPage();
-
-    } else {
-      console.log("localStorage does not exist");
-      $getJSON("/data/portfolioJSON.json", function(data){
-        localStorage.setItem ('project', JSON.stringify(data));
-        Portfolioproject.loadAll(data);
-        portfolioView.initPage();
-      })
-    }
+    })
+  }
 
 
-  };
+};
+
+Portfolioproject.loadAll= function(projects) {
+  projects.forEach(function(project) {
+    Portfolioproject.all.push(new Portfolioproject(project));
+  })
+};
